4316808ad4c53084a4ebae9f71f70345dc123049
unknown <betim009@gmail.com>
2025-02-19 15:20:48 -0300

    update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,13 @@
 import './header.css'
 import MyNavBar from "../my-navBar";
+import logo from '/logo.png'
 
 export default function Header(params) {
     return (
         <header className='my-header'>
-            <h3>App</h3>
+            <h3>
+                <img className='logo-app' src={logo} alt="" />
+            </h3>
 
             <MyNavBar />
         </header>
--- src/components/my-navBar/index.jsx
+++ src/components/my-navBar/index.jsx
@@ -5,8 +5,7 @@ export default function MyNavBar() {
   return (
     <nav className="my-nav">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/template">Another link</NavLink>
+      <NavLink to="/cart">Sua Compra</NavLink>
     </nav>
   );
 }
