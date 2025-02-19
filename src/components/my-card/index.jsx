8e30c22fe1e177b7db44020a43883a803febdda1
unknown <betim009@gmail.com>
2025-02-19 15:07:07 -0300

    update

---
+++ src/components/my-card/index.jsx
@@ -0,0 +1,50 @@
+import './myCard.css'
+import useCart from "../../hooks/useCart";
+import Button from 'react-bootstrap/Button';
+import { Card, Col, Row } from "react-bootstrap";
+
+export default function MyCard({ array }) {
+    const { handlePlus, handleMinus } = useCart();
+
+    return (
+        <>
+            {
+                array.map(e => (
+                    <Col lg={6} key={e.id} className="mb-2">
+                        <Card>
+                            <Row>
+                                <Col sm={12} md={6} lg={5} className='col-thumb'>
+                                    <img src={e.urlImg} alt="notFound" class="img-thumb rounded-start" />
+                                </Col>
+                                <Col sm={12} md={6} lg={7} className='col-body'>
+                                    <h5 className="card-title">{e.name}</h5>
+                                    <p className="card-text">{e.details}</p>
+                                    <p className="card-text">
+                                        <h4 className="text-body-secondary">
+                                            {e.price}
+                                        </h4>
+                                    </p>
+                                    <div className="div-cart">
+                                        <Button
+                                            variant="danger"
+                                            onClick={() => handlePlus(e)}
+                                        >
+                                            +
+                                        </Button>
+                                        <p className='text-price'>{e.count}</p>
+                                        <Button
+                                            variant="danger"
+                                            onClick={() => handleMinus(e)}
+                                        >
+                                            -
+                                        </Button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                ))
+            }
+        </>
+    )
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import { Card, Col, Row } from "react-bootstrap";
 import Accordion from 'react-bootstrap/Accordion';
+import MyCard from "../../components/my-card";
 
 
 export default function Home() {
@@ -15,48 +16,14 @@ export default function Home() {
   return (
     <main>
       <Container>
-        <Accordion defaultActiveKey={['0']} alwaysOpen>
+        <Accordion className="mt-5" defaultActiveKey={['0']} alwaysOpen>
           <Accordion.Item eventKey="0">
             <Accordion.Header>
               <h4>Lanches</h4>
             </Accordion.Header>
             <Accordion.Body>
-              <Row className="mt-5">
-                {dataLanches.map(e => (
-                  <Col lg={6} key={e.id} className="mb-2">
-                    <Card>
-                      <Row>
-                        <Col sm={12} md={6} lg={5}>
-                          <img src={e.urlImg} alt="notFound" class="img-thumb rounded-start" />
-                        </Col>
-                        <Col sm={12} md={6} lg={7}>
-                          <h5 className="card-title">{e.name}</h5>
-                          <p className="card-text">{e.details}</p>
-                          <p className="card-text">
-                            <small className="text-body-secondary">
-                              {e.price}
-                            </small>
-                          </p>
-                          <div className="d-flex gap-3">
-                            <Button
-                              variant="danger"
-                              onClick={() => handlePlus(e)}
-                            >
-                              +
-                            </Button>
-                            <h3>{e.count}</h3>
-                            <Button
-                              variant="danger"
-                              onClick={() => handleMinus(e)}
-                            >
-                              -
-                            </Button>
-                          </div>
-                        </Col>
-                      </Row>
-                    </Card>
-                  </Col>
-                ))}
+              <Row>
+                <MyCard array={dataLanches} />
               </Row>
             </Accordion.Body>
           </Accordion.Item>
@@ -65,47 +32,17 @@ export default function Home() {
               <h4>Bebidas</h4>
             </Accordion.Header>
             <Accordion.Body>
-              <Row className="mt-5">
-                {dataBebidas.map(e => (
-                  <Col lg={6} key={e.id} className="mb-2">
-                    <Card>
-                      <Row>
-                        <Col sm={12} md={6} lg={5}>
-                          <img src={e.urlImg} alt="notFound" class="img-thumb rounded-start" />
-                        </Col>
-                        <Col sm={12} md={6} lg={7}>
-                          <h5 className="card-title">{e.name}</h5>
-                          <p className="card-text">{e.details}</p>
-                          <p className="card-text">
-                            <small className="text-body-secondary">
-                              {e.price}
-                            </small>
-                          </p>
-                          <div className="d-flex gap-3">
-                            <Button
-                              variant="danger"
-                              onClick={() => handlePlus(e)}
-                            >
-                              +
-                            </Button>
-                            <h3>{e.count}</h3>
-                            <Button
-                              variant="danger"
-                              onClick={() => handleMinus(e)}
-                            >
-                              -
-                            </Button>
-                          </div>
-                        </Col>
-                      </Row>
-                    </Card>
-                  </Col>
-                ))}
+              <Row>
+                <MyCard array={dataBebidas} />
               </Row>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
-
+        <div className="d-grid gap-2 mt-5">
+          <Button variant="danger" size="lg">
+            Finalizar Compra
+          </Button>
+        </div>
 
 
       </Container>
