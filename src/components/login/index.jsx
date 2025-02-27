31c902c387924b9956c541460ca0f1f1add0dd6a
unknown <betim009@gmail.com>
2025-02-27 11:28:52 -0300

    update

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,19 +1,9 @@
-import { Button, Col, Row } from "react-bootstrap";
-import Form from 'react-bootstrap/Form';
-
+import './login.css'
+import { Button, Col, Container, Form } from "react-bootstrap";
 export default function Login() {
     return (
-        <Form className="mt-5">
-            <Row style={{ backgroundColor: "#ededed", padding: "10px", borderRadius: "10px" }}>
-                <Form.Group
-                    as={Col}
-                    md={12} lg={5}
-                    className="mb-3"
-                    controlId="formGroupName"
-                >
-                    <Form.Label>Nome</Form.Label>
-                    <Form.Control type="text" placeholder="nome completo" />
-                </Form.Group>
+        <div className="div-login">
+            <Form>
                 <Form.Group
                     as={Col}
                     md={12} lg={5}
@@ -25,59 +15,20 @@ export default function Login() {
                 </Form.Group>
                 <Form.Group
                     as={Col}
-                    md={12} lg={2}
+                    md={12} lg={5}
                     className="mb-3"
-                    controlId="formGroupTel"
+                    controlId="formGroupSenha"
                 >
-                    <Form.Label>Telefone</Form.Label>
-                    <Form.Control type="tel" placeholder="27 99881234" />
-                </Form.Group>
-                <Form.Group
-                    as={Col}
-                    md={12} lg={6}
-                    className="mb-3"
-                    controlId="formGroupPassword">
                     <Form.Label>Senha</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="sua senha" />
                 </Form.Group>
-                <Form.Group
-                    as={Col}
-                    md={12} lg={6}
-                    className="mb-3"
-                    controlId="formGroupPassword2">
-                    <Form.Label>Repetir senha</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group
-                    as={Col}
-                    md={12} lg={10}
-                    className="mb-3"
-                    controlId="formGroupAddress">
-                    <Form.Label>Endereço</Form.Label>
-                    <Form.Control type="text" placeholder="Avenida..." />
-                    <Form.Text className="text-muted">
-                        Estabelicimento da cidade de Vitória/ES.
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group
-                    as={Col}
-                    md={12} lg={2}
-                    className="mb-3"
-                    controlId="formGroupNumber">
-                    <Form.Label>Nº</Form.Label>
-                    <Form.Control type="number" placeholder="15" />
-                </Form.Group>
-
-                <Col>
-                    <Button
-                        variant="danger"
-                        type="submit"
-                        className="mb-2">
-                        Cadastrar
-                    </Button>
-                </Col>
-            </Row>
-        </Form>
-
+                <Button
+                    variant="danger"
+                    size="md"
+                >
+                    Entrar
+                </Button>
+            </Form>
+        </div>
     )
 }
--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,12 +1,14 @@
-import { Col, Container, Form } from "react-bootstrap";
-import Login from "../../components/login";
-import { useContext, useEffect } from "react";
+import { Button, Col, Container, Form } from "react-bootstrap";
+import SingUp from "../../components/singUp";
+import { useContext, useEffect, useState } from "react";
 import Context from "../../context/Context";
 import { Link } from "react-router-dom";
 import './signIn.css'
+import Login from "../../components/login";
 
 export default function SignIn() {
     const { userValidate, setUserValidate } = useContext(Context);
+    const [display, setDisplay] = useState(true);
 
     useEffect(() => {
         if (!userValidate.start) {
@@ -16,47 +18,29 @@ export default function SignIn() {
     }, [])
 
     const handleClickQuestion = () => {
-        alert('')
+        setDisplay((prev) => !prev)
     }
 
-    function SingUp() {
-        return (
-            <>
-                <Form>
-                    <Form.Group
-                        as={Col}
-                        md={12} lg={5}
-                        className="mb-3"
-                        controlId="formGroupEmail"
-                    >
-                        <Form.Label>E-mail</Form.Label>
-                        <Form.Control type="email" placeholder="seu@email.com" />
-                    </Form.Group>
-                    <Form.Group
-                        as={Col}
-                        md={12} lg={5}
-                        className="mb-3"
-                        controlId="formGroupSenha"
-                    >
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="sua senha" />
-                    </Form.Group>
-                </Form>
-            </>
-        )
-    }
 
     return (
         <main>
-            <Container>
-                {userValidate.email ? SingUp() : <Login />}
+            <Container className="container-signIn">
+                {display ? <Login /> : <SingUp />}
 
-                <div className="div-question">
-                    <p>Você já possui uma conta?
-                        {" "}
-                        <Link onClick={handleClickQuestion} className="text-danger">Faça Login</Link>
-                    </p>
-                </div>
+                {display ?
+                    <div className="div-question">
+                        <p>Não possui uma conta?
+                            {" "}
+                            <Link onClick={handleClickQuestion} className="text-danger">Faça o seu cadastro</Link>
+                        </p>
+                    </div>
+                    :
+                    <div className="div-question">
+                        <p>Você já possui uma conta?
+                            {" "}
+                            <Link onClick={handleClickQuestion} className="text-danger">Faça Login</Link>
+                        </p>
+                    </div>}
             </Container>
         </main>
     )
